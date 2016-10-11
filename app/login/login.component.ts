edf88171d21667964f6cc6a8703b40e74209f416
Dev-Min <miny7050@gmail.com>
2016-10-11 17:17:54 +0900

    #func - growl 추가

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -3,16 +3,38 @@
 */
 import {Component} from '@angular/core';
 import { Router }            from '@angular/router';
+import { Message } from 'primeng/primeng';
 
 @Component({
 selector: 'my-app',
 templateUrl: 'app/login/login.component.html'
 })
 export class LoginComponent {
+    msgs: Message[] = [];
+    userId: string;
+    userPw: string;
+
     constructor(private router: Router) { }
     
     onLogin() {
-        let link = ['/userList'];
-        this.router.navigate(link);
+        if(this.userId == null || this.userId == '') {
+            this.showError('아이디를');
+        }
+        else if(this.userPw == null || this.userPw == '') {
+            this.showError('비밀번호를');
+        }
+        else {
+            let link = ['/userList'];
+            this.router.navigate(link);
+        }
+    }
+    
+    showError(title: string) {
+        this.msgs = [];
+        this.msgs.push({severity:'error', summary:title + ' 입력해주세요!', detail:'Validation failed'});
+    }
+
+    hide() {
+        this.msgs = [];
     }
 }
