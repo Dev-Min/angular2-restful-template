4f90fbb5afc196f053819e55d97ad44738c8f656
Dev-Min <miny7050@gmail.com>
2016-10-11 17:07:10 +0900

    #func - growl추가

--- app/user/user.modification/user.modification.component.ts
+++ app/user/user.modification/user.modification.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit }          from '@angular/core';
 import { Headers, Http }              from '@angular/http';
 import { Location }               from '@angular/common';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { SelectItem } from 'primeng/primeng';
+import { SelectItem, Message } from 'primeng/primeng';
 
 import { UserService } from '../user.service/user.service';
 import { User } from '../user.model/user';
@@ -19,6 +19,7 @@ import { Dept } from '../user.model/dept';
 })
 
 export class UserModificationComponent implements OnInit {
+    msgs: Message[] = [];
     depts: Dept[] = [];
     items: SelectItem[] = [];
     userTitle = "User Modification";
@@ -40,17 +41,35 @@ export class UserModificationComponent implements OnInit {
                 .then( user => {
                     this.user = user;
                     this.depts = this.service.getDepts();
-                    this.items.push({ label: this.user.dept.deptNameType, value: this.user.dept });
+                    this.items.push( { label: this.user.dept.deptNameType, value: this.user.dept });
                     for ( let dept of this.depts ) {
                         this.items.push( { label: dept.deptNameType, value: { deptId: dept.deptId, deptNameType: dept.deptNameType } });
-//                        this.user.dept = this.items[user.dept.deptId - 1].value;
+                        //                        this.user.dept = this.items[user.dept.deptId - 1].value;
                     }
                 });
         });
     }
 
     onUpdateUser(): void {
-        this.service.update( this.user ).then(() => this.goBack() );
+        if ( this.user.email == '' ) {
+            this.showError( "E-Mail을" );
+        }
+        else if ( this.user.password == '' ) {
+            this.showError( "Password를" );
+        }
+        else if ( this.user.name == '' ) {
+            this.showError( "Name을" );
+        }
+        else if ( this.user.age == 0 ) {
+            this.showError( "Age를" );
+        }
+        else if ( this.user.dept.deptNameType == null ) {
+            this.showError( "DEPT를" );
+        }
+        else {
+            this.showInfo();
+            this.service.update( this.user ).then(() => this.goBack() );
+        }
     }
 
     onDeleteUser(): void {
@@ -62,9 +81,23 @@ export class UserModificationComponent implements OnInit {
         this.router.navigate( link );
     }
 
-    onChangeDropdown(dept: SelectItem) {
-//      this.user.dept.deptId = dept.value.deptId;
-//      this.user.dept.deptNameType = dept.value.deptNameType;
-      this.user.dept = this.items[dept.value.deptId - 1].value;
-  }
+    onChangeDropdown( dept: SelectItem ) {
+        //      this.user.dept.deptId = dept.value.deptId;
+        //      this.user.dept.deptNameType = dept.value.deptNameType;
+        this.user.dept = this.items[dept.value.deptId - 1].value;
+    }
+
+    showError( title: string ) {
+        this.msgs = [];
+        this.msgs.push( { severity: 'error', summary: title + ' 입력해주세요!', detail: 'Validation failed' });
+    }
+
+    hide() {
+        this.msgs = [];
+    }
+
+    showInfo() {
+        this.msgs = [];
+        this.msgs.push( { severity: 'submitted', summary: '회원 추가 성공', detail: 'PrimeNG rocks' });
+    }
 }
